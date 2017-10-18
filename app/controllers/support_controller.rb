8e421b22796d61e45f9f4b7ad932faf2fd9377fe
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-18 11:47:46 +1000

    Messages sanitized

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -8,8 +8,8 @@ class SupportController < ApplicationController
     message = params[:message]
 
     @errors = []
-    @errors << 'YAINT, DAT EMAIL EMPTY' if from_email.blank?
-    @errors << 'SAY WHAT AGAIN' if message.blank?
+    @errors << 'Error: Please enter an e-mail address' if from_email.blank?
+    @errors << 'Error: Please enter a message to send' if message.blank?
 
     if @errors.empty?
       SupportMailer.contact_support(from_email, message).deliver_now
